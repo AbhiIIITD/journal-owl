4b9794f091af1c8d96c1c049cf5ea820fda5a38d
Abhishek Bansal <abhishek22021@iiitd.ac.in>
2024-05-23 00:09:06 +0530

    commit

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('52eaed2966f4438aad1b0e0c44743c12');
+
+const getTopHeadlines = async () => {
+    const response = await newsapi.v2.topHeadlines({
+        language: 'en',
+        country: 'in'
+    });
+    return response;
+}
+
+
+router.get('/', async (req, res) => {
+    const top_headlines = await getTopHeadlines()
+    let top12 = top_headlines.articles.slice(0, 12)
+
+    top12 = top12.map((element) => {
+        return { ...element, publishedAt: element.publishedAt.slice(0, 10)}
+    })
+
+    res.render('home', {
+        headlines: top12
+    })
+})
+
+module.exports = router
